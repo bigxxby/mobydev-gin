1dba512ca0da29253ff92332a619a37bb8784eb5
bigxxby <bigxxby@yandex.ru>
2024-04-12 20:43:17 +0500

    explanation

--- internal/start/start.go
+++ internal/start/start.go
@@ -44,31 +44,33 @@ func Start() {
 	router.GET("/create/movie", main.GET_HTML_Movie)
 
 	// movies
-	router.GET("/api/movies", main.GET_Movies)
-	router.GET("/api/movies/:id", main.GET_Movie)
-	router.GET("/api/movies/season/:id", main.GET_Season)
-	router.GET("/api/movies/episode/:id", main.GET_Episode)
+	router.GET("/api/movies", main.GET_Movies)              //get all movies
+	router.GET("/api/movies/:id", main.GET_Movie)           // get movie by id
+	router.GET("/api/movies/season/:id", main.GET_Season)   //get season by id
+	router.GET("/api/movies/episode/:id", main.GET_Episode) // get episode by id
 
-	router.POST("/api/movies", main.POST_Movie)
+	router.POST("/api/movies", main.POST_Movie) //create movie (ADMIN ONLY)
 
-	router.DELETE("/api/movies/:id", main.DELETE_Movie)
+	router.DELETE("/api/movies/:id", main.DELETE_Movie) // delete movie by id (ADMIN ONLY)
 
-	router.PUT("/api/movies/:id", main.PUT_Movie)
+	router.PUT("/api/movies/:id", main.PUT_Movie) //change movie by id (ADMIN ONLY)
 
 	// profile
-	router.GET("/api/profile", main.GET_Profile)
+	router.GET("/api/profile", main.GET_Profile) // get profile of CURRENT USER (auth)
+	// get all profiles
+	// get profile by id etc.
 
 	// trends
-	router.GET("/api/trends/:id", main.GET_Trend)
-	router.GET("/api/trends", main.GET_Trends)
+	router.GET("/api/trends/:id", main.GET_Trend) // get trend by id
+	router.GET("/api/trends", main.GET_Trends)    // get all current trends
 
 	// reg/log
-	router.POST("/api/reg", main.POST_Reg)
-	router.POST("/api/login", main.POST_Login)
+	router.POST("/api/reg", main.POST_Reg)     // body{email,password,role}
+	router.POST("/api/login", main.POST_Login) // body{email,password}
 
 	//favorites
-	router.GET("/api/favorites", main.GET_Favorites)              //all favorites of CURRENT USER(auth)
-	router.POST("/api/favorites/:id", main.POST_Favorite)         //add to fav by movieId to CURRENT USER
+	router.GET("/api/favorites", main.GET_Favorites)              //all favorites of CURRENT USER (auth)
+	router.POST("/api/favorites/:id", main.POST_Favorite)         //add to fav by movieId of CURRENT USER (auth)
 	router.DELETE("/api/favorites/:id", main.DELETE_Favorite)     //delete from favorites of CURRENT USER (auth) by movie id
 	router.DELETE("/api/favorites/clear/", main.DELETE_Favorites) //clears all CURRENT USERS favorites
 
