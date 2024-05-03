bc7bd2371666fa07ae2a43652e834aa8558009c6
bigxxby <bigxxby@yandex.ru>
2024-05-03 15:25:31 +0500

    little fix

--- pkg/routes/api/age/GET.go
+++ pkg/routes/api/age/GET.go
@@ -35,17 +35,16 @@ func (m *AgeRoute) GET_AgeCategories(c *gin.Context) {
 	c.JSON(200, ageCategories)
 }
 
-// GET_AgeCategory retrieves an age category by ID
-//
-//	@Summary		Get an age category by ID
-//	@Description	Retrieves an age category with the specified ID
-//	@Produce		json
-//	@Param			id	path		int								true	"Age Category ID"
-//	@Success		200	{object}	AgeCategory						"OK"
-//	@Failure		400	{object}	routes.DefaultMessageResponse	"Bad request"
-//	@Failure		404	{object}	routes.DefaultMessageResponse	"Age category not found"
-//	@Failure		500	{object}	routes.DefaultMessageResponse	"Internal Server Error"
-//	@Router			/api/age-categories/{id} [get]
+// @Tags			ageCategory
+// @Summary		Get an age category by ID
+// @Description	Retrieves an age category with the specified ID
+// @Produce		json
+// @Param			id	path		int								true	"Age Category ID"
+// @Success		200	{object}	AgeCategory						"OK"
+// @Failure		400	{object}	routes.DefaultMessageResponse	"Bad request"
+// @Failure		404	{object}	routes.DefaultMessageResponse	"Age category not found"
+// @Failure		500	{object}	routes.DefaultMessageResponse	"Internal Server Error"
+// @Router			/api/age-categories/{id} [get]
 func (m *AgeRoute) GET_AgeCategory(c *gin.Context) {
 	ageId := c.Param("id")
 
